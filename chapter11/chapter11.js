08441e2793fbe3ef8d98ef539031b66aced9107f
CodePrewsh <91918136+CodePrewsh@users.noreply.github.com>
2023-09-06 16:16:05 +0200

    Add files via upload

---
+++ chapter11/chapter11.js
@@ -0,0 +1,30 @@
+let people = ["Precious",
+"Lizzy",
+"Bonga",
+"Musa",
+"Landon"];
+
+let otherPeople = ["Teddy",
+"Carla",
+"Judy",
+"Eddie"];
+
+// people = people.concat(otherPeople);
+// people = people.indexOf("Landon");
+// people = people.join(" # ");
+// people = people.lastIndexOf("Lizzy");
+// people = people.pop();
+// people.pop();
+// people = people.push("Bonga");
+// people.push("Teddy");
+// people = people.reverse();
+// people.shift();
+// // people.unshift("Teddy");
+// people = people.slice(0,3);
+// people = people.sort();
+// people.splice(1,0,"Cathy");
+
+
+document.getElementById("peopleIKnow").innerHTML = people.valueOf();
+document.getElementById("peopleIKnow").innerHTML =people.toString();
+
